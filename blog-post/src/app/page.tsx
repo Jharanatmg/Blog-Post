b4fb430caad7b5293d02cff07c7f4615b9c3af58
Kabina Thapa <thapakabina28@gmail.com>
2023-08-02 17:18:56 -0400

    Fetch data on homepage

--- blog-post/src/app/page.tsx
+++ blog-post/src/app/page.tsx
@@ -1,7 +1,33 @@
-import Image from 'next/image'
+'use client'
+import React, {useEffect} from 'react'
+import {useDispatch, useSelector} from 'react-redux'
+import { fetchPosts } from '@/redux/features/dataSlice'
+import { AppDispatch, RootState } from '@/redux/store'
 
 export default function Home() {
+  const dispatch=useDispatch<AppDispatch>()
+  const{data, status, error}=useSelector((state: RootState)=>state.posts)
+  console.log(data)
+ 
+  useEffect(()=>{
+    dispatch(fetchPosts())
+  },[])
+  if(status==='loading'){
+    return <p>Loading...</p>
+  }
+  if(status==='failed'){
+    return<p>Error:{error}</p>
+  }
+
+  
+
   return (
-   <div>hi</div>
+   <div>
+    {data.map((post)=>(
+      <li key={post.id}>
+        <h1>{post.title}</h1>
+      </li>
+    ))}
+   </div>
   )
 }
