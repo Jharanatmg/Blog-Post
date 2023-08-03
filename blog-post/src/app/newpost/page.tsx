1669e681fc8355fd9c2abbd71b957c2bf8cc0e90
Kabina Thapa <thapakabina28@gmail.com>
2023-08-03 17:36:29 -0400

    Implement authorizarion to access create new post page

--- blog-post/src/app/newpost/page.tsx
+++ blog-post/src/app/newpost/page.tsx
@@ -1,20 +1,27 @@
 "use client";
 import React from "react";
 import Navbar from "@/components/navbar";
-import Protectedroutes from '@/components/protectedroutes';
+import Protected from '@/components/protected';
+import { useSelector } from "react-redux";
+import { RootState } from "@/redux/store";
 
 
 function page() {
-  <Protectedroutes>
-  return <div className="bg-white w-full h-screen">
+  const{data, status, error}=useSelector((state: RootState)=>state.posts)
+  
+  return (
+  <Protected>
+    <div className="bg-white w-full h-screen">
     <Navbar/>
+    <img src={data[0]?.image} width='500'/>
     <input type="text" placeholder="enter title"/>
     <input type="text" placeholder="enter author"/>
     <input type="text" placeholder="enter description"/>
     <input type="text" placeholder="enter date"/>
     <input type="text" placeholder="enter image"/>
-  </div>;
-  </Protectedroutes>
+  </div>
+  </Protected>
+  )
 }
 
 export default page;
--- blog-post/src/components/navbar.tsx
+++ blog-post/src/components/navbar.tsx
@@ -1,7 +1,26 @@
 import React from "react";
 import Link from "next/link";
+import { useRouter } from "next/navigation";
+import { toast } from 'react-toastify';
+import { ToastContainer } from 'react-toastify';
+import "react-toastify/dist/ReactToastify.css";
 
 function navbar() {
+  const router=useRouter()
+  const handleLogout=()=>{
+    localStorage.removeItem('session-token')
+    router.push('/signin')
+  }
+  const handlenewpost=()=>{
+    const token=localStorage.getItem('session-token')
+    if(token){
+      router.push('/newpost')
+    }
+    else{
+      
+      toast.error("please login to post a new post.")
+    }
+  }
   return <div className="flex w-full justify-between p-4">
     <ul>
       <li>Stanton</li>
@@ -11,9 +30,11 @@ function navbar() {
       <li>About</li>
       <li>Contact</li>
       <li>Shop</li>
-      <li><Link href="/newpost">Create New post</Link></li>
+      <button onClick={handlenewpost}>Create New post</button>
       <li>Login</li>
+      <button onClick={handleLogout}>Logout</button>
     </ul>
+    <ToastContainer/>
   </div>;
 }
 
