abe684e5d526a488894e04d0e8b41f62392ea96f
Jharanatmg <jharanatmg22@gmail.com>
2023-08-03 21:09:17 -0700

    homepage

--- blog-post/src/redux/features/dataSlice.tsx
+++ blog-post/src/redux/features/dataSlice.tsx
@@ -1,55 +1,54 @@
-import React from 'react'
-import {createSlice, createAsyncThunk, PayloadAction} from '@reduxjs/toolkit'
+import React from "react";
+import { createSlice, createAsyncThunk, PayloadAction } from "@reduxjs/toolkit";
 
-const JSONURL='http://localhost:4001/posts'
+const JSONURL = "http://localhost:4001/posts";
 
-export const fetchPosts= createAsyncThunk<Post[],void,{}>('posts/fetchPosts', async()=>{
-  const response= await fetch(JSONURL)
-  const data=await response.json()
-  return data as Post[]
-})
-interface Post{
-  id:number,
-  
-  title:string,
-  description:string,
-  author:string,
-  date:string,
-  image:string,
+export const fetchPosts = createAsyncThunk<Post[], void, {}>(
+  "posts/fetchPosts",
+  async () => {
+    const response = await fetch(JSONURL);
+    const data = await response.json();
+    return data as Post[];
+  }
+);
+interface Post {
+  id: number;
 
+  title: string;
+  description: string;
+  author: string;
+  date: string;
+  image: string;
 }
-interface PostState{
-  data:Post[],
-  status:'idle'|'loading'|'succeeded'|'failed'
-  error:string|null
-
+interface PostState {
+  data: Post[];
+  status: "idle" | "loading" | "succeeded" | "failed";
+  error: string | null;
 }
-const initialState:PostState={
-  data:[],
-  status:'idle',
-  error:null,
-}
+const initialState: PostState = {
+  data: [],
+  status: "idle",
+  error: null,
+};
 
- const dataSlice = createSlice({
-  name:'posts',
+const dataSlice = createSlice({
+  name: "posts",
   initialState,
 
-  reducers:{},
-  extraReducers:(builder)=>{
+  reducers: {},
+  extraReducers: (builder) => {
     builder
-    .addCase(fetchPosts.pending, (state)=>{
-      state.status='loading'
-    })
-    .addCase(fetchPosts.fulfilled, (state, action:PayloadAction<Post[]>) => {
-      state.status = 'succeeded';
-      state.data = action.payload;
-    })
-    .addCase(fetchPosts.rejected, (state, action) => {
-      state.status = 'failed';
-      state.error = action.error.message;
-    });
-  }
-})
-export default dataSlice.reducer
-  
-
+      .addCase(fetchPosts.pending, (state) => {
+        state.status = "loading";
+      })
+      .addCase(fetchPosts.fulfilled, (state, action: PayloadAction<Post[]>) => {
+        state.status = "succeeded";
+        state.data = action.payload;
+      })
+      .addCase(fetchPosts.rejected, (state, action) => {
+        state.status = "failed";
+        state.error = action.error.message;
+      });
+  },
+});
+export default dataSlice.reducer;
